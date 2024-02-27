0d0c18b503f195d9fcd6b4f3a9d6de1ff2d508b6
minhquantb00 <minhquantb00@gmail.com>
2024-02-27 15:39:13 +0700

    init

---
+++ WebCourseManagement_Business/Interfaces/IKhoaHocService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebCourseManagement_Models.RequestModels.KhoaHocRequests;
+using WebCourseManagement_Models.ResponseModels.DataKhoaHoc;
+using WebCourseManagement_Models.Responses;
+
+namespace WebCourseManagement_Business.Interfaces
+{
+    public interface IKhoaHocService
+    {
+        Task<ResponseObject<DataResponseKhoaHoc>> ThemKhoaHoc(Request_ThemKhoaHoc request);
+    }
+}
