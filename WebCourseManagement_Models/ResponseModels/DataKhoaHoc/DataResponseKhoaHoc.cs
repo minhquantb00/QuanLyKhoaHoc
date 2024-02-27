b5c69de08410db8272f64a47fdebf4c9810fc6cf
minhquantb00 <minhquantb00@gmail.com>
2024-02-27 15:31:33 +0700

    init

--- WebCourseManagement_Models/ResponseModels/DataKhoaHoc/DataResponseKhoaHoc.cs
+++ WebCourseManagement_Models/ResponseModels/DataKhoaHoc/DataResponseKhoaHoc.cs
@@ -1,31 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WebCourseManagement_Models.Entities;
+using WebCourseManagement_Models.ResponseModels.DataNguoiDung;
 
-namespace WebCourseManagement_Models.Entities
+namespace WebCourseManagement_Models.ResponseModels.DataKhoaHoc
 {
-    public class KhoaHoc : BaseEntity
+    public class DataResponseKhoaHoc
     {
         public string TenKhoaHoc { get; set; }
         public string MoTa { get; set; }
         public string AnhKhoaHoc { get; set; }
         public int SoBaiHoc { get; set; }
         public int SoChuong { get; set; }
-        public int NguoiTaoId { get; set; }
         public decimal GiaKhoaHoc { get; set; }
-        public virtual NguoiDung NguoiTao { get; set; }
+        public  DataResponseNguoiDung NguoiTao { get; set; }
         public DateTime NgayTao { get; set; }
         public DateTime? NgayCapNhat { get; set; }
         public int TongThoiGianBaiHoc { get; set; }
-        public int SoNguoiDangKyKhoaHoc { get; set ; }
+        public int SoNguoiDangKyKhoaHoc { get; set; }
         public int SoHocVienDaHoanThanh { get; set; }
-        public int TrangThaiKhoaHocId { get; set; }
-        public virtual TrangThaiKhoaHoc TrangThaiKhoaHoc { get; set; }
-        public int LoaiKhoaHocId { get; set; }
-        public virtual LoaiKhoaHoc LoaiKhoaHoc { get; set; }
+        public string TenLoaiKhoaHoc { get; set; }
         public string? LinkVideoGioiThieu { get; set; }
     }
 }
