182ddbb8cb1c9d9eb9e034784e9bd716de087355
quanghuy-mgr87 <91078298+quanghuy-mgr87@users.noreply.github.com>
2024-02-27 21:49:54 +0700

    init

---
+++ WebCourseManagement_Business/Implements/KhoaHocService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebCourseManagement_Business.Interfaces;
+using WebCourseManagement_Models.RequestModels.KhoaHocRequests;
+using WebCourseManagement_Models.ResponseModels.DataKhoaHoc;
+using WebCourseManagement_Models.Responses;
+
+namespace WebCourseManagement_Business.Implements
+{
+    public class KhoaHocService : IKhoaHocService
+    {
+        public Task<ResponseObject<DataResponseKhoaHoc>> ThemKhoaHoc(Request_ThemKhoaHoc request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
