a713a643f88aab9091e95a7f3a35be4c7d4c5804
minhquantb00 <minhquantb00@gmail.com>
2024-02-29 17:13:40 +0700

    init

---
+++ WebCourseManagement_Models/ResponseModels/DataBaiHoc/DataResponseBaiHoc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebCourseManagement_Models.Entities;
+
+namespace WebCourseManagement_Models.ResponseModels.DataBaiHoc
+{
+    public class DataResponseBaiHoc
+    {
+        public string TenBaiHoc { get; set; }
+        public string MoTaBaiHoc { get; set; }
+        public DateTime ThoiGianTao { get; set; }
+        public DateTime? ThoiGianCapNhat { get; set; }
+        public int ThoiLuongVideo { get; set; }
+    }
+}
