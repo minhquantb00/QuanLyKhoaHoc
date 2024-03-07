56adb2b5de0257bb053dbe0630cc8566a1da6678
minhquantb00 <minhquantb00@gmail.com>
2024-03-07 16:47:17 +0700

    init

---
+++ WebCourseManagement_API/Controllers/AuthController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebCourseManagement_Business.Interfaces;
+using WebCourseManagement_Models.RequestModels.AuthRequests;
+
+namespace WebCourseManagement_API.Controllers
+{
+    [Route("api/auth")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+        [HttpPost("DangKy")]
+        public async Task<IActionResult> DangKy([FromForm] Request_DangKy request)
+        {
+            var result = await _authService.DangKy(request);
+            switch(result.Status)
+            {
+                case 200:
+                    return Ok(result);
+                case 404:
+                    return NotFound(result);
+                case 400: 
+                    return BadRequest(result);
+                case 401:
+                    return Unauthorized(result);
+                case 403:
+                    return Forbid("Bạn không có quyền thực hiện chức năng này");
+                default:
+                    return StatusCode(500, result);
+            }
+        }
+
+        [HttpPost("DangNhap")]
+        public async Task<IActionResult> DangNhap([FromBody] Request_DangNhap request)
+        {
+            var result = await _authService.DangNhap(request);
+            switch (result.Status)
+            {
+                case 200:
+                    return Ok(result);
+                case 404:
+                    return NotFound(result);
+                case 400:
+                    return BadRequest(result);
+                case 401:
+                    return Unauthorized(result);
+                case 403:
+                    return Forbid("Bạn không có quyền thực hiện chức năng này");
+                default:
+                    return StatusCode(500, result);
+            }
+        }
+
+        [HttpPut("XacThucDangKyTaiKhoan")]
+        public async Task<IActionResult> XacThucDangKyTaiKhoan([FromBody] Request_XacNhanDangKyTaiKhoan request)
+        {
+            var result = await _authService.XacNhanDangKyTaiKhoan(request);
+            switch (result.Status)
+            {
+                case 200:
+                    return Ok(result);
+                case 404:
+                    return NotFound(result);
+                case 400:
+                    return BadRequest(result);
+                case 401:
+                    return Unauthorized(result);
+                case 403:
+                    return Forbid("Bạn không có quyền thực hiện chức năng này");
+                default:
+                    return StatusCode(500, result);
+            }
+        }
+    }
+}
--- WebCourseManagement_Business/Implements/AuthService.cs
+++ WebCourseManagement_Business/Implements/AuthService.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Net.Mail;
+using System.Net;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using WebCourseManagement_Business.Interfaces;
@@ -9,6 +13,16 @@ using WebCourseManagement_Models.DataContexts;
 using WebCourseManagement_Models.RequestModels.AuthRequests;
 using WebCourseManagement_Models.ResponseModels.DataNguoiDung;
 using WebCourseManagement_Models.Responses;
+using WebCourseManagement_Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using WebCourseManagement_Models.Entities;
+using BcryptNet = BCrypt.Net.BCrypt;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using Microsoft.Extensions.Configuration;
+using WebCourseManagement_Commons.DefaultConstants;
 
 namespace WebCourseManagement_Business.Implements
 {
@@ -18,24 +32,199 @@ namespace WebCourseManagement_Business.Implements
         private readonly ResponseObject<DataResponseNguoiDung> _responseObject;
         private readonly ResponseObject<DataResponseToken> _responseTokenObject;
         private readonly NguoiDungConverter _converter;
-        public AuthService(AppDbContext context, ResponseObject<DataResponseNguoiDung> responseObject, ResponseObject<DataResponseToken> responseTokenObject, NguoiDungConverter converter)
+        private readonly IConfiguration _configuration;
+        public AuthService(AppDbContext context, ResponseObject<DataResponseNguoiDung> responseObject, ResponseObject<DataResponseToken> responseTokenObject, NguoiDungConverter converter, IConfiguration configuration)
         {
             _context = context;
             _responseObject = responseObject;
             _responseTokenObject = responseTokenObject;
             _converter = converter;
+            _configuration = configuration;
         }
-
-        public Task<ResponseObject<DataResponseNguoiDung>> DangKy(Request_DangKy request)
+        #region Chức năng GenerateRefreshToken
+        public string GenerateRefreshToken()
         {
-            throw new NotImplementedException();
+            var random = new byte[32];
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(random);
+                return Convert.ToBase64String(random);
+            }
         }
-
-        public Task<ResponseObject<DataResponseToken>> DangNhap(Request_DangNhap request)
+        #endregion
+        #region Chức năng gửi email
+        public string SendEmail(EmailTo emailTo)
         {
-            throw new NotImplementedException();
-        }
+            if (!Utilities.IsValidEmail(emailTo.To))
+            {
+                return "Định dạng email không hợp lệ";
+            }
+            var smtpClient = new SmtpClient("smtp.gmail.com")
+            {
+                Port = 587,
+                Credentials = new NetworkCredential("minhquantb00@gmail.com", "jvztzxbtyugsiaea"),
+                EnableSsl = true
+            };
+            try
+            {
+                var message = new MailMessage();
+                message.From = new MailAddress("minhquantb00@gmail.com");
+                message.To.Add(emailTo.To);
+                message.Subject = emailTo.Subject;
+                message.Body = emailTo.Content;
+                message.IsBodyHtml = true;
+                smtpClient.Send(message);
 
+                return "Xác nhận gửi email thành công, lấy mã để xác thực";
+            }
+            catch (Exception ex)
+            {
+                return "Lỗi khi gửi email: " + ex.Message;
+            }
+        }
+        #endregion
+        #region Chức năng GenerateCodeActive
+        private int GenerateCodeActive()
+        {
+            Random random = new Random();
+            return random.Next(100000, 999999);
+        }
+        #endregion
+        #region Chức năng đăng ký
+        public async Task<ResponseObject<DataResponseNguoiDung>> DangKy(Request_DangKy request)
+        {
+            var nguoiDung = await _context.nguoiDungs.SingleOrDefaultAsync(x => x.TaiKhoan.Equals(request.TaiKhoan));
+            if(string.IsNullOrWhiteSpace(request.TaiKhoan) || string.IsNullOrWhiteSpace(request.MatKhau) || string.IsNullOrWhiteSpace(request.HoVaTen) || string.IsNullOrWhiteSpace(request.SoDienThoai) || string.IsNullOrWhiteSpace(request.Email))
+            {
+                return _responseObject.ResponseError(StatusCodes.Status400BadRequest, "Vui lòng điền đầy đủ thông tin", null);
+            }
+            if (!Utilities.IsValidEmail(request.Email))
+            {
+                return _responseObject.ResponseError(StatusCodes.Status400BadRequest, "Định dạng email không hợp lệ", null);
+            }
+            if (!Utilities.IsValidPhoneNumber(request.SoDienThoai))
+            {
+                return _responseObject.ResponseError(StatusCodes.Status400BadRequest, "Định dạng số điện thoại không hợp lệ", null);
+            }
+            if(nguoiDung != null)
+            {
+                return _responseObject.ResponseError(StatusCodes.Status400BadRequest, "Tên tài khoản đã được sử dụng", null);
+            }
+            if(_context.nguoiDungs.Any(x => x.Email.Equals(request.Email)))
+            {
+                return _responseObject.ResponseError(StatusCodes.Status400BadRequest, "Email đã được sử dụng", null);
+            }
+            if (_context.nguoiDungs.Any(x => x.Email.Equals(request.SoDienThoai)))
+            {
+                return _responseObject.ResponseError(StatusCodes.Status400BadRequest, "Số điện thoại đã được sử dụng", null);
+            }
+            NguoiDung item = new NguoiDung
+            {
+                AnhDaiDien = "https://scontent.xx.fbcdn.net/v/t1.15752-9/423737674_1480040725879365_4588731426501580823_n.png?stp=dst-png_s206x206&_nc_cat=101&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeGxnP82_YkVFLWwGazF7yI5KZjnfOEJczUpmOd84QlzNRfr9gdH6syoper3Yh0oOyo5y2ipmbd59Dwk6Ozyz536&_nc_ohc=eJwRiZR1zw0AX94Q9w9&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_AdSAsCug3mmQNHjjqLBlE4vswrK6MxAOKWbXdPwaEh6CfA&oe=66109EB0",
+                IsActive = true,
+                DaKhoa = false,
+                Email = request.Email,
+                GioiTinh = request.GioiTinh,
+                TaiKhoan = request.TaiKhoan,
+                HoVaTen = request.HoVaTen,
+                MatKhau = BcryptNet.HashPassword(request.MatKhau),
+                NgaySinh = request.NgaySinh,
+                SoDienThoai = request.SoDienThoai,
+                QuyenHanId = 3,
+                ThoiGianTao = DateTime.Now,
+                TrangThaiNguoiDungId = 1,
+            };
+             _context.nguoiDungs.Add(item);
+            _context.SaveChanges();
+            XacNhanEmail xacNhanEmail = new XacNhanEmail
+            {
+                DaXacNhan = false,
+                MaXacNhan = "MyBugs_" + GenerateCodeActive().ToString(),
+                NguoiDungId = item.Id,
+                ThoiGianHetHan = DateTime.Now.AddHours(8)
+            };
+             _context.xacNhanEmails.Add(xacNhanEmail);
+            _context.SaveChanges();
+            string message = SendEmail(new EmailTo
+            {
+                To = item.Email,
+                Subject = "Nhận mã xác nhận để đăng ký tài khoản tại đây:",
+                Content = $"Mã xác nhận của bạn là: {xacNhanEmail.MaXacNhan}"
+            });
+            return _responseObject.ResponseSuccess("Đăng ký tài khoản thành công", _converter.EntityToDTO(item));
+        }
+        #endregion
+        #region GenerateAccessToken
+        public DataResponseToken GenerateAccessToken(NguoiDung nguoiDung)
+        {
+            var jwtTokenHandler = new JwtSecurityTokenHandler();
+            var secretKeyBytes = System.Text.Encoding.UTF8.GetBytes(_configuration.GetSection(Constants.AppSettingKeys.AUTH_SECRET).Value!);
+            var role = _context.quyenHans.SingleOrDefault(x => x.Id == nguoiDung.QuyenHanId);
+
+            var tokenDescription = new SecurityTokenDescriptor
+            {
+                Subject = new System.Security.Claims.ClaimsIdentity(new[]
+                {
+                    new Claim("Id", nguoiDung.Id.ToString()),
+                    new Claim("Email", nguoiDung.Email),
+                    new Claim("Name", nguoiDung.HoVaTen),
+                    new Claim("Username", nguoiDung.TaiKhoan),
+                    new Claim(ClaimTypes.Role, role?.MaQuyenHan ?? "")
+                }),
+                Expires = DateTime.Now.AddHours(4),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(secretKeyBytes), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = jwtTokenHandler.CreateToken(tokenDescription);
+            var accessToken = jwtTokenHandler.WriteToken(token);
+            var refreshToken = GenerateRefreshToken();
+
+            RefreshToken rf = new RefreshToken
+            {
+                Token = refreshToken,
+                ThoiGianHetHan = DateTime.Now.AddHours(10),
+                NguoiDungId = nguoiDung.Id
+            };
+
+            _context.refreshTokens.Add(rf);
+            _context.SaveChanges();
+
+            DataResponseToken data = new DataResponseToken
+            {
+                AccessToken = accessToken,
+                RefreshToken = refreshToken,
+                DataResponseNguoiDung = _converter.EntityToDTO(nguoiDung)
+            };
+            return data;
+        }
+        #endregion
+        #region Chức năng đăng nhập
+        public async Task<ResponseObject<DataResponseToken>> DangNhap(Request_DangNhap request)
+        {
+            var nguoiDung = await _context.nguoiDungs.SingleOrDefaultAsync(x => x.TaiKhoan.Equals(request.TaiKhoan));
+            if(string.IsNullOrWhiteSpace(request.TaiKhoan) || string.IsNullOrWhiteSpace(request.MatKhau))
+            {
+                return _responseTokenObject.ResponseError(StatusCodes.Status400BadRequest, "Vui lòng điền đầy đủ thông tin", null);
+            }
+            if(nguoiDung == null)
+            {
+                return _responseTokenObject.ResponseError(StatusCodes.Status400BadRequest, "Sai tên tài khoản", null);
+            }
+            bool checkPass = BcryptNet.Verify(request.MatKhau, nguoiDung.MatKhau);
+            if(!checkPass)
+            {
+                return _responseTokenObject.ResponseError(StatusCodes.Status400BadRequest, "Mật khẩu không chính xác", null);
+            }
+            if(nguoiDung.TrangThaiNguoiDungId == 1)
+            {
+                return _responseTokenObject.ResponseError(StatusCodes.Status400BadRequest, "Tài khoản người dùng chưa được kích hoạt! Vui lòng kích hoạt tài khoản để sử dụng", null);
+            }
+            if(nguoiDung.IsActive == false || nguoiDung.DaKhoa == true)
+            {
+                return _responseTokenObject.ResponseError(StatusCodes.Status400BadRequest, "Tài khoản đã bị khóa hoặc bị vô hiệu! Vui lòng thử lại", null);
+            }
+            return _responseTokenObject.ResponseSuccess("Đăng nhập thành công", GenerateAccessToken(nguoiDung));
+        }
+        #endregion
         public Task<string> DoiMatKhau(Request_DoiMatKhau request)
         {
             throw new NotImplementedException();
@@ -51,9 +240,22 @@ namespace WebCourseManagement_Business.Implements
             throw new NotImplementedException();
         }
 
-        public Task<ResponseObject<DataResponseNguoiDung>> XacNhanDangKyTaiKhoan(Request_XacNhanDangKyTaiKhoan request)
+        public async Task<ResponseObject<DataResponseNguoiDung>> XacNhanDangKyTaiKhoan(Request_XacNhanDangKyTaiKhoan request)
         {
-            throw new NotImplementedException();
+            var confirmEmail = await _context.xacNhanEmails.SingleOrDefaultAsync(x => x.MaXacNhan.Equals(request.MaXacNhan));
+            if(confirmEmail == null)
+            {
+                return _responseObject.ResponseError(StatusCodes.Status404NotFound, "Mã xác nhận không chính xác", null);
+            }
+            var nguoiDung = await _context.nguoiDungs.SingleOrDefaultAsync(x => x.Id == confirmEmail.NguoiDungId);
+            if(confirmEmail.ThoiGianHetHan < DateTime.Now)
+            {
+                return _responseObject.ResponseError(StatusCodes.Status400BadRequest, "Mã xác nhận đã hết hạn", null);
+            }
+            nguoiDung.TrangThaiNguoiDungId = 2;
+             _context.SaveChanges();
+
+            return _responseObject.ResponseSuccess("Kích hoạt tài khoản thành công", _converter.EntityToDTO(nguoiDung));
         }
 
         public Task<string> XacNhanQuenMatKhau(Request_XacNhanQuenMatKhau request)
