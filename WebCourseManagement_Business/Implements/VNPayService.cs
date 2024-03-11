bbe32efe4500c2d6e112029d0e4bcbb8a53d770c
minhquantb00 <minhquantb00@gmail.com>
2024-03-11 10:56:18 +0700

    init

--- WebCourseManagement_Business/Implements/VNPayService.cs
+++ WebCourseManagement_Business/Implements/VNPayService.cs
@@ -7,26 +7,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using WebCourseManagement_Business.Interfaces;
+using WebCourseManagement_Models.ConfigModels.VnPayPayment;
 using WebCourseManagement_Models.DataContexts;
 using WebCourseManagement_Models.Entities;
 using WebCourseManagement_Repositories;
 using WebCourseManagement_Repositories.HandleVNPayPayment;
+using Utils = WebCourseManagement_Models.ConfigModels.VnPayPayment.Utils;
 
 namespace WebCourseManagement_Business.Implements
 {
     public class VNPayService : IVNPayService
     {
         private readonly AppDbContext _context;
-        private readonly VNPayLibrary pay;
         private readonly IConfiguration _configuration;
         private readonly AuthService _authService;
-        public VNPayService(AppDbContext context, VNPayLibrary pay, IConfiguration configuration, AuthService authService)
+        private readonly DataVnpay _vnPay;
+        public VNPayService(AppDbContext context, IConfiguration configuration, AuthService authService, DataVnpay vnPay)
         {
             _context = context;
-            this.pay = pay;
             _configuration = configuration;
             _authService = authService;
+            _vnPay = vnPay;
         }
+
         public async Task<string> TaoDuongDanThanhToan(int hoaDonId, HttpContext httpContext, int id)
         {
             var hoaDon = await _context.hoaDonDangKies.SingleOrDefaultAsync(x => x.Id == hoaDonId);
@@ -39,20 +42,22 @@ namespace WebCourseManagement_Business.Implements
                 }
                 if (hoaDon.TrangThaiHoaDonId == 1 && hoaDon.TongTien != 0 && hoaDon.TongTien != null)
                 {
-                    pay.AddRequestData("vnp_Version", "2.1.0");
-                    pay.AddRequestData("vnp_Command", "pay");
-                    pay.AddRequestData("vnp_TmnCode", "YIK14C5R");
-                    pay.AddRequestData("vnp_Amount", (hoaDon.TongTien * 1000).ToString());
-                    pay.AddRequestData("vnp_CreateDate", DateTime.Now.ToString("yyyyMMddHHmmss"));
-                    pay.AddRequestData("vnp_CurrCode", "VND");
-                    pay.AddRequestData("vnp_IpAddr", Utils.GetIpAddress(httpContext));
-                    pay.AddRequestData("vnp_Locale", "vn");
-                    pay.AddRequestData("vnp_OrderInfo", $"Thanh toán hóa đơn: {hoaDonId}");
-                    pay.AddRequestData("vnp_OrderType", "other");
-                    pay.AddRequestData("vnp_ReturnUrl", _configuration.GetSection("VnPay:vnp_ReturnUrl").Value);
-                    pay.AddRequestData("vnp_TxnRef", hoaDonId.ToString());
+                    VnPayLibrary vnpay = new VnPayLibrary();
 
-                    string duongDanThanhToan = pay.CreateRequestUrl(_configuration.GetSection("VnPay:vnp_Url").Value, _configuration.GetSection("VnPay:vnp_HashSecret").Value);
+                    vnpay.AddRequestData("vnp_Version", "2.1.0");
+                    vnpay.AddRequestData("vnp_Command", "pay");
+                    vnpay.AddRequestData("vnp_TmnCode", "1FL9AZ4R");
+                    vnpay.AddRequestData("vnp_Amount", (hoaDon.TongTien * 1000).ToString());
+                    vnpay.AddRequestData("vnp_CreateDate", hoaDon.ThoiGianTao.ToString("yyyyMMddHHmmss"));
+                    vnpay.AddRequestData("vnp_CurrCode", "VND");
+                    vnpay.AddRequestData("vnp_IpAddr", Utils.GetIpAddress(httpContext));
+                    vnpay.AddRequestData("vnp_Locale", "vn");
+                    vnpay.AddRequestData("vnp_OrderInfo", "Thanh toán hóa đơn: " + hoaDon.Id);
+                    vnpay.AddRequestData("vnp_OrderType", "other");
+                    vnpay.AddRequestData("vnp_ReturnUrl", "https://localhost:7046/api/user/Return");
+                    vnpay.AddRequestData("vnp_TxnRef", hoaDon.Id.ToString());
+
+                    string duongDanThanhToan = vnpay.CreateRequestUrl(_configuration.GetSection("VnPay:BaseUrl").Value, _configuration.GetSection("VnPay:HashSecret").Value);
                     return duongDanThanhToan;
                 }
 
---
+++ WebCourseManagement_Models/ConfigModels/VnPayPayment/DataVnpay.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebCourseManagement_Models.ConfigModels.VnPayPayment
+{
+    public class DataVnpay
+    {
+        public string? TmnCode { get; set; }
+        public string? HashSecret { get; set; }
+        public string? ReturnUrl { get; set; }
+        public string? BaseUrl { get; set; }
+        public string? Command { get; set; }
+        public string? CurrCode { get; set; }
+        public string? Version { get; set; }
+        public string? Locale { get; set; }
+    }
+}
